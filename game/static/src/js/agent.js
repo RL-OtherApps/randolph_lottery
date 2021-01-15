6b3812ed0c4fc7dcb64ad45bde9be5d232c37eed
root <ketal.planetodoo@gmail.com>
2021-01-15 19:02:47 +0530

    chanegs in lottery

--- game/static/src/js/agent.js
+++ game/static/src/js/agent.js
@@ -22,3 +22,27 @@ document.getElementById('ifYes').style.display = 'none';
  document.getElementById('ifno').style.display = 'block';
 }
 }
+function walletValidation()
+{
+amount = document.getElementById('amount').value;
+wallet = document.getElementById('amt')
+wallet2 = document.getElementById('amt2')
+if(wallet2){
+if (amount>wallet2.value){
+alert("Sorry!!, You don't have that much amount in your wallet")
+return false
+} else
+{
+return true;
+}
+}
+if(wallet){
+if (amount>wallet.value){
+alert("Sorry!!, You don't have that much amount in your wallet")
+return false
+} else
+{
+return true;
+}
+}
+}
